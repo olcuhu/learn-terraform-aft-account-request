287b91630ea23585c2666404e5f423ba4a0adffc
olcuhu <ollie.cuffley-hur@control-plane.io>
2024-02-22 11:51:33 +0000

    Create sandbox-aft CT account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "oliver.cuffley+learnaft-sandbox@gmail.com"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    SSOUserEmail              = "oliver.cuffley+awsdevopsgeneral@gmail.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
